c9c28f68e3c55ae20516377ee7fe23ea84ac1909
Owez <root@ogriffihts.com>
2020-08-09 22:18:43 +0100

    Renamed for future plugin support

--- src/router/index.js
+++ src/router/index.js
@@ -44,11 +44,11 @@ Vue.use(VueRouter)
   },
   {
     path: '/editor',
-    name: 'Editor',
+    name: 'DocEditor',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Editor.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/DocEditor.vue')
   }
 ]
 
---
+++ src/views/DocEditor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="editor">
+    <Splashscreen title="Editor coming soon.." subtitle="We're working on it, we promise!" />
+  </div>
+</template>
+
+<script>
+import Splashscreen from "@/components/Splashscreen.vue";
+
+export default {
+  name: "DocEditor",
+  components: {
+    Splashscreen,
+  },
+  data() {
+    // temporary, should fetch id given in query
+    return {
+      doc: {
+        id: 1,
+        title: "This is my first document",
+        tagline: "First few characters or custom short description",
+        content: "The long content part of this thing!",
+      },
+    };
+  },
+};
+</script>
